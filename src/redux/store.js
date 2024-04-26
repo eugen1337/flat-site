7f6cc3be0851b3f164900f28921046f011bd92f2
eugen1337 <evgenijkurakin478@gmail.com>
2024-04-26 14:57:55 +0700

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
 import authReducer from "./slices/authReducer";
 import planReducer from "./slices/planReducer";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
-    reducer: {
-        auth: authReducer,
-        plan: planReducer
-    },
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    plan: planReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
+export default store;
--- src/state/builder.js
+++ src/state/builder.js
@@ -1,8 +1,15 @@
-import store from "../redux/store";
+import store, { persistor } from "../redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default () => {
     return (props) => {
-        return <Provider store={store}>{props.children}</Provider>;
+        return (
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    {props.children}
+                </PersistGate>
+            </Provider>
+        );
     };
 };
